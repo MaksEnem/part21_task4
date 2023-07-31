1f1bbe25154226f789ce2df26c1ef5ed5fb49507
MaksEnem <110655471+MaksEnem@users.noreply.github.com>
2023-07-31 14:15:17 +1100

    therd

--- task4.cpp
+++ task4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 struct Person {
 
@@ -7,76 +8,304 @@ struct Person {
 	int health_points;
 	int armor;
 	int damage;
-
+	int coordinate_x;
+	int coordinate_y;
 };
 
-void initializing_heroy(Person& heroy) {
+void initializing_opponents(Person& opponents, int &i) {	
+	
+		opponents.name = "Opponent#" + std::to_string(i + 1);
+		opponents.health_points = std::rand() % 101 + 50;
+		opponents.armor = std::rand() % 51;
+		opponents.damage = std::rand() % 16 + 15;	
 
-	std::cout << "Enter your character's name: ";
-	std::cin >> heroy.name;
-
-	std::cout << "Enter the number of health points for your character: ";
-	std::cin >> heroy.health_points;
-
-	std::cout << "Enter the number of armor for your character: ";
-	std::cin >> heroy.armor;
-
-	std::cout << "Enter the number of damage for your character: ";
-	std::cin >> heroy.damage;
 }
 
-void initializing_fieid(bool is_field[][20]) {
+void initializing_heroy(Person& opponents) {
+	
+	std::cout << "Enter your character's name: ";
+	std::cin >> opponents.name;
+
+	std::cout << "Enter the number of health points for your character: ";
+	std::cin >> opponents.health_points;
+
+	std::cout << "Enter the number of armor for your character: ";
+	std::cin >> opponents.armor;
+
+	std::cout << "Enter the number of damage for your character: ";
+	std::cin >> opponents.damage;
+}
+
+void initializing_fieid(char field[][20]) {
 
 	for (int i = 0; i < 20; ++i) {
 		for (int j = 0; j < 20; ++j) {
 
-			is_field[i][j] = false;
+			field[i][j] = '.';
 		}
 	}
+}
+
+void initializing_game_fieid_opponents(Person& opponents, char field[][20]) {
+
+		do {
+
+			opponents.coordinate_x = std::rand() % 20;
+			opponents.coordinate_y = std::rand() % 20;
+
+		} while (field[opponents.coordinate_x][opponents.coordinate_y] != '.');
+
+		field[opponents.coordinate_x][opponents.coordinate_y] = 'E';
+
+}
+
+void initializing_game_fieid_heroy(Person& opponents, char field[][20]) {
+
+	do {
+
+		opponents.coordinate_x = 10;
+		opponents.coordinate_y = 10;
+
+	} while (field[opponents.coordinate_x][opponents.coordinate_y] != '.');
+
+	field[opponents.coordinate_x][opponents.coordinate_y] = 'P';
+
+}
+
+void playing_field(char field[][20]) {
+
+	for (int i = 0; i < 20; ++i) {
+		for (int j = 0; j < 20; ++j) {
+
+			if (field[i][j] == '.') {
+
+				std::cout << "." << " ";
+
+			}
+			else if (field[i][j] == 'E') {
+
+				std::cout << "E" << " ";
+
+			}
+			else if (field[i][j] == 'P') {
+
+				std::cout << "P" << " ";
+
+			}
+
+		}
+		std::cout << std::endl;
+	}
+
 }
 
 int main() {
 
 	std::srand(std::time(nullptr));
 
-	Person opponents[5];	
-	Person heroy;
 
-	for (int i = 0; i < 5; ++i) {
+	Person opponents[6];	
 
-		opponents[i].name = "Opponent#" + std::to_string(i + 1);
-		opponents[i].health_points = std::rand() % 101 + 50;
-		opponents[i].armor = std::rand() % 51;
-		opponents[i].damage = std::rand() % 16 + 15;
+
+	for (int i = 0; i < 5; ++i) {			
+
+		initializing_opponents(opponents[i], i);
 	}
 
 
+	initializing_heroy(opponents[5]);
 
-	initializing_heroy(heroy);
+
+	 char field[20][20];
+
+
+	initializing_fieid(field);
+
+
+	for (int i = 0; i < 5; ++i) {
+
+		initializing_game_fieid_opponents(opponents[i], field);
+	}	
+
+	
+	initializing_game_fieid_heroy(opponents[5], field);
+
+	playing_field(field);
 
 	
 
 
-	bool is_field[20][20];
-
-	initializing_fieid(is_field);
 
 
 
 
 
-	for (int i = 0; i < 20; ++i) {
-		for (int j = 0; j < 20; ++j) {
 
-			if (is_field[i][j] == 0) {
+	bool is_result = true;
 
-				std::cout << "." << " ";
+	while (is_result) {
+
+		int step;
+		char step_heroy;
+
+
+		for (int i = 0; i < 6; ++i) {
+
+			if (i < 5) {
+
+
+				step = std::rand() % 4;
+
+				if (step == 0) {
+
+					if (opponents[i].coordinate_x == 19) {
+
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'E';
+
+					}
+					else {
+
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = '.';
+
+						opponents[i].coordinate_x += 1;
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'E';
+
+					}
+				}
+				else if (step == 1) {
+
+
+					if (opponents[i].coordinate_x == 0) {
+
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'E';
+
+					}
+					else {
+
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = '.';
+
+						opponents[i].coordinate_x -= 1;
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'E';
+
+					}
+				}
+				else if (step == 2) {
+
+					if (opponents[i].coordinate_y == 19) {
+
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'E';
+
+					}
+					else {
+
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = '.';
+
+						opponents[i].coordinate_y += 1;
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'E';
+
+					}
+				}
+				else if (step == 3) {
+
+					if (opponents[i].coordinate_y == 0) {
+
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'E';
+
+					}
+					else {
+
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = '.';
+
+						opponents[i].coordinate_y -= 1;
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'E';
+
+					}
+				}
 
 			}
-			
-		}
-		std::cout << std::endl;
-	}
+			else if (i == 5) {
 
+				do {
+
+					std::cout << "Make a move with the keys: l, r, u, d: ";
+					std::cin >> step_heroy;
+
+				} while (step_heroy != 'l' && step_heroy != 'r' && step_heroy != 'u' && step_heroy != 'd');
+
+
+				if (step_heroy == 'l') {
+
+					if (opponents[i].coordinate_y == 0) {
+
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'P';
+
+					}
+					else {
+
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = '.';
+
+						opponents[i].coordinate_y -= 1;
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'P';
+
+					}
+				}
+				else if (step_heroy == 'r') {
+
+					if (opponents[i].coordinate_y == 19) {
+
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'P';
+
+					}
+					else {
+
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = '.';
+
+						opponents[i].coordinate_y += 1;
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'P';
+
+					}
+
+				}
+				else if (step_heroy == 'u') {
+
+					if (opponents[i].coordinate_x == 0) {
+
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'P';
+
+					}
+					else {
+
+						field[opponents[i].coordinate_x][opponents[5].coordinate_y] = '.';
+
+						opponents[i].coordinate_x -= 1;
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'P';
+
+					}
+				}
+				else if (step_heroy == 'd') {
+
+					if (opponents[i].coordinate_x == 19) {
+
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'P';
+
+					}
+					else {
+
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = '.';
+
+						opponents[i].coordinate_x += 1;
+						field[opponents[i].coordinate_x][opponents[i].coordinate_y] = 'P';
+
+					}
+				}
+				std::cout << std::endl;
+
+				playing_field(field);
+
+			}
+
+		}
+
+	}
 
 }
